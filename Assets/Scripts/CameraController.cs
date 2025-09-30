6f9dc3abd203309ab9d8082868619a343dff7d00
tsang0072 <tsang0072@icloud.com>
2025-09-30 14:58:40 +0100

    CameraIn
    door2 bug unfixed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
     public static CameraController instance;
-    public Transform player;
-    private Vector3 offset;
+    // public Transform player;
+    // private Vector3 offset;
+    public CinemachineVirtualCamera roomCam;
+    private CinemachineVirtualCamera followCam;
     
      void Awake() {
         if(!instance){
@@ -18,12 +21,25 @@ public class CameraController : MonoBehaviour
     }
     void Start()
     {
-        offset=transform.position-player.position;
+        //offset=transform.position-player.position;
+        followCam = GameObject.Find("VCam_Follow").GetComponent<CinemachineVirtualCamera>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        transform.position=offset+player.position;
+        if (other.CompareTag("Player"))
+        {
+            roomCam.Priority = 20;   // take control
+            followCam.Priority = 10; 
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            roomCam.Priority = 0; 
+            followCam.Priority = 20;   
+        }
     }
 }
