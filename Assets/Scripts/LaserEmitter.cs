46c8cc34e22f0707d7b404f7b89c4568f4d05b16
tsang0072 <tsang0072@icloud.com>
2025-10-10 17:21:46 +0100

    LaserCamera in

---
+++ Assets/Scripts/LaserEmitter.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserEmitter : MonoBehaviour
+{
+    [Header("Laser Settings")]
+    public Transform laserOrigin;
+    public float laserLength = 10f;
+    public Color laserColor = Color.red;
+    public float lineWidth = 0.05f;
+    public LayerMask hitLayers;
+
+    [Header("Damage Settings")]
+    public bool lethalToPlayer = true;
+
+    private LineRenderer lr;
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.positionCount = 2;
+        lr.startWidth = lineWidth;
+        lr.endWidth = lineWidth;
+        lr.material = new Material(Shader.Find("Unlit/Color"));
+        lr.material.color = laserColor;
+
+        if (laserOrigin == null)
+            laserOrigin = transform;
+    }
+
+    void Update()
+    {
+        EmitLaser();
+    }
+
+    void EmitLaser()
+    {
+         Vector3 origin = laserOrigin.position;
+        Vector3 direction = laserOrigin.forward;
+
+        Ray ray = new Ray(origin, direction);
+        RaycastHit hit;
+
+        Vector3 endPosition;
+
+        if (Physics.Raycast(ray, out hit, laserLength, hitLayers))
+        {
+            endPosition = hit.point;
+
+            if (lethalToPlayer && hit.collider.CompareTag("Player"))
+            {
+                Debug.Log("Player hit by laser!");
+                // hit.collider.GetComponent<PlayerDeath>()?.Die();
+            }
+        }
+        else
+        {
+            endPosition = origin + direction * laserLength;
+        }
+
+        lr.SetPosition(0, origin);
+        lr.SetPosition(1, endPosition);
+    }
+}
